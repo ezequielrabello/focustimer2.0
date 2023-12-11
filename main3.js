109b4c1b1880269cac80b1ae7d08d81a7cb0c7af
Ezequiel Rabello <137337652+Kielbatera@users.noreply.github.com>
2023-12-11 13:06:17 -0300

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,58 @@
+const display = window.document.getElementById('display')
+let buttons = window.document.getElementById('controls')
+let intervalId = 0 
+let duration = 0
+
+
+function setTimer(duration) {
+    let minutes = Number(document.getElementById('minutes').value)
+    let seconds = Number(document.getElementById('seconds').value)
+    duration = (minutes * 60) + seconds
+
+    minutes = Math.floor(duration / 60)
+    seconds = Math.floor(duration % 60)
+
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2,"0")}`
+}
+
+function playPause() {
+    
+    const play = document.getElementById('play')
+    const pause = document.getElementById('pause')
+    const action = buttons.getAttribute('action')  
+
+    clearInterval(intervalId)
+
+    if (action == 'start' || action == 'continue') {
+
+        intervalId = setInterval(() => {
+            duration--
+            formatTime(duration)
+
+        }, 1000)
+        
+        document.documentElement.classList.toggle('running')
+        pause.setAttribute('action', 'pause')
+    } else if (action == 'pause') {
+        document.documentElement.classList.toggle('running')
+        play.setAttribute('action', 'continue')
+    } 
+
+    
+}
+
+function formatTime(time) {
+    display.innerText = setTimer(time)
+}
+
+function resetTime() {
+    clearInterval(intervalId)
+    duration = 0
+    formatTime(duration)
+    const play = document.getElementById('play')
+    play.setAttribute('action', 'start') 
+    document.documentElement.classList.toggle('running')
+}
+
+
+
